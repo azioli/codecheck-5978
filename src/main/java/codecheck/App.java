72aa7d979436cbeb124a47123dd91ab454e55dcf
DESKTOP-0RAT5Q6\LuGuanyu <guanyulu@usc.edu>
2017-02-05 18:25:48 -0800

    App

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,7 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println("Hello "+args[i]+"!");
+			System.out.println("Hello"+args[i]+"!");
 		}
 	}
 }
